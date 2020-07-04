07d9027d46b273865836fcf2965290cdc40d46c9
Sebastian Draus <sebastian.draus@nordicsemi.no>
2020-07-04 22:17:21 +0200

    REVERT: Port basic CLI commands

--- subsys/zigbee/cli/zigbee_cli.c
+++ subsys/zigbee/cli/zigbee_cli.c
@@ -1,21 +1,28 @@
-/*$$$LICENCE_NORDIC_STANDARD<2018>$$$*/
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <shell/shell.h>
+
 #include "zigbee_cli.h"
 
 /* CLI Agent endpoint */
 static zb_uint8_t cli_ep;
 
-/* Counter timer. */
-APP_TIMER_DEF(m_timer_0);
+// /* Counter timer. */
+// APP_TIMER_DEF(m_timer_0);
 
-#ifdef ZIGBEE_CLI_DEBUG
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
 /* Debug mode indicator. */
 static zb_bool_t m_debug_mode = ZB_FALSE;
 #endif
 
-#ifdef ZIGBEE_CLI_DEBUG
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
 /* Zigbee stack processing suspension indicator. */
 static zb_bool_t m_suspended = ZB_FALSE;
 #endif
@@ -24,52 +31,51 @@ static zb_bool_t m_suspended = ZB_FALSE;
  */
 zb_uint8_t zb_get_cli_endpoint(void)
 {
-    return cli_ep;
+	return cli_ep;
 }
 
-#ifdef ZIGBEE_CLI_DEBUG
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
 /**@brief Sets the debug mode.
  */
 zb_void_t zb_cli_debug_set(zb_bool_t debug)
 {
-    m_debug_mode = debug;
+	m_debug_mode = debug;
 }
 #endif
 
-#ifdef ZIGBEE_CLI_DEBUG
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
 /**@brief Gets the debug mode.
  */
 zb_bool_t zb_cli_debug_get(zb_void_t)
 {
-    return m_debug_mode;
+	return m_debug_mode;
 }
 #endif
 
-#ifdef ZIGBEE_CLI_DEBUG
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
 /**@brief Function for suspending the processing of the Zigbee main loop.
  */
 zb_void_t zb_cli_suspend(zb_void_t)
 {
-    m_suspended = ZB_TRUE;
+	m_suspended = ZB_TRUE;
 }
 #endif
 
-#ifdef ZIGBEE_CLI_DEBUG
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
 /**@brief Function for resuming the processing of the Zigbee main loop.
  */
 zb_void_t zb_cli_resume(zb_void_t)
 {
-    m_suspended = ZB_FALSE;
+	m_suspended = ZB_FALSE;
 }
 #endif
 
-#ifdef ZIGBEE_CLI_DEBUG
-/**@brief Function for getting the state of the Zigbee stack processing suspension.
+#ifdef CONFIG_ZIGBEE_SHELL_DEBUG_CMD
+/**@brief Function for getting the state of the Zigbee stack
+ *        processing suspension.
  */
 zb_bool_t zb_cli_stack_is_suspended(zb_void_t)
 {
-    return m_suspended;
+	return m_suspended;
 }
 #endif
-
-/** @} */
--- subsys/zigbee/cli/zigbee_cli_utils.c
+++ subsys/zigbee/cli/zigbee_cli_utils.c
@@ -1,11 +1,21 @@
-/*$$$LICENCE_NORDIC_STANDARD<2018>$$$*/
-#include "nrf_cli.h"
-#include "zboss_api.h"
-#include "zb_error_handler.h"
-#include "zigbee_logger_eprxzcl.h"
-#include "zigbee_cli_utils.h"
-#include <ctype.h>
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <zephyr/types.h>
+#include <shell/shell.h>
+
+#include <zboss_api.h>
+#include <zb_error_handler.h>
+#include <zigbee_logger_eprxzcl.h>
+#include "zigbee_cli_utils.h"
+
+/* TODO: REMOVE
 NRF_SECTION_DEF(zb_ep_handlers, zb_device_handler_t);
 #define ZB_EP_HANDLER_SECTION_ITEM_GET(i) NRF_SECTION_ITEM_GET(zb_ep_handlers, zb_device_handler_t, (i))
 #define ZB_EP_HANDLER_SECTION_ITEM_COUNT  NRF_SECTION_ITEM_COUNT(zb_ep_handlers, zb_device_handler_t)
@@ -29,124 +39,132 @@ zb_uint8_t cli_agent_ep_handler(zb_bufid_t bufid)
 
     return ZB_FALSE;
 }
+*/ //TODO: REMOVE
 
-int zcl_attr_to_str(char * p_str_buf, uint16_t buf_len, zb_uint16_t attr_type, zb_uint8_t * p_attr)
+int zcl_attr_to_str(char * p_str_buf, u16_t buf_len, zb_uint16_t attr_type,
+		    zb_uint8_t * p_attr)
 {
-    int bytes_written = 0;
-    int string_len;
-    int i;
+	int bytes_written = 0;
+	int string_len;
+	int i;
 
-    if ((p_str_buf == NULL) || (p_attr == NULL))
-    {
-        return -1;
-    }
+	if ((p_str_buf == NULL) || (p_attr == NULL)) {
+		return -1;
+	}
 
-    switch (attr_type)
-    {
-        /* Boolean */
-        case ZB_ZCL_ATTR_TYPE_BOOL:
-            bytes_written = snprintf(p_str_buf, buf_len, "%s", *((zb_bool_t *)p_attr) ? "True" : "False");
-            break;
+	switch (attr_type) {
+	/* Boolean */
+	case ZB_ZCL_ATTR_TYPE_BOOL:
+		bytes_written = snprintf(p_str_buf, buf_len, "%s",
+					 *((zb_bool_t *)p_attr) ? "True"
+						: "False");
+		break;
 
-        /* 1 byte */
-        case ZB_ZCL_ATTR_TYPE_8BIT:
-        case ZB_ZCL_ATTR_TYPE_8BITMAP:
-        case ZB_ZCL_ATTR_TYPE_U8:
-        case ZB_ZCL_ATTR_TYPE_8BIT_ENUM:
-            bytes_written = snprintf(p_str_buf, buf_len, "%hu", *((zb_uint8_t*)p_attr));
-            break;
+	/* 1 byte */
+	case ZB_ZCL_ATTR_TYPE_8BIT:
+	case ZB_ZCL_ATTR_TYPE_8BITMAP:
+	case ZB_ZCL_ATTR_TYPE_U8:
+	case ZB_ZCL_ATTR_TYPE_8BIT_ENUM:
+		bytes_written = snprintf(p_str_buf, buf_len, "%hu",
+					 *((zb_uint8_t*)p_attr));
+		break;
 
-        case ZB_ZCL_ATTR_TYPE_S8:
-            bytes_written = snprintf(p_str_buf, buf_len, "%hd", *((zb_int8_t*)p_attr));
-            break;
+	case ZB_ZCL_ATTR_TYPE_S8:
+		bytes_written = snprintf(p_str_buf, buf_len, "%hd",
+					 *((zb_int8_t*)p_attr));
+		break;
 
-        /* 2 bytes */
-        case ZB_ZCL_ATTR_TYPE_16BIT:
-        case ZB_ZCL_ATTR_TYPE_16BITMAP:
-        case ZB_ZCL_ATTR_TYPE_U16:
-        case ZB_ZCL_ATTR_TYPE_16BIT_ENUM:
-            bytes_written = snprintf(p_str_buf, buf_len, "%hu", *((zb_uint16_t*)p_attr));
-            break;
+	/* 2 bytes */
+	case ZB_ZCL_ATTR_TYPE_16BIT:
+	case ZB_ZCL_ATTR_TYPE_16BITMAP:
+	case ZB_ZCL_ATTR_TYPE_U16:
+	case ZB_ZCL_ATTR_TYPE_16BIT_ENUM:
+		bytes_written = snprintf(p_str_buf, buf_len, "%hu",
+					 *((zb_uint16_t*)p_attr));
+		break;
 
-        case ZB_ZCL_ATTR_TYPE_S16:
-            bytes_written = snprintf(p_str_buf, buf_len, "%hd", *((zb_int16_t*)p_attr));
-            break;
+	case ZB_ZCL_ATTR_TYPE_S16:
+		bytes_written = snprintf(p_str_buf, buf_len, "%hd",
+					 *((zb_int16_t*)p_attr));
+		break;
 
-        /* 4 bytes */
-        case ZB_ZCL_ATTR_TYPE_32BIT:
-        case ZB_ZCL_ATTR_TYPE_32BITMAP:
-        case ZB_ZCL_ATTR_TYPE_U32:
-            bytes_written = snprintf(p_str_buf, buf_len, "%u", *((zb_uint32_t*)p_attr));
-            break;
+	/* 4 bytes */
+	case ZB_ZCL_ATTR_TYPE_32BIT:
+	case ZB_ZCL_ATTR_TYPE_32BITMAP:
+	case ZB_ZCL_ATTR_TYPE_U32:
+		bytes_written = snprintf(p_str_buf, buf_len, "%u",
+					 *((zb_uint32_t*)p_attr));
+		break;
 
-        case ZB_ZCL_ATTR_TYPE_S32:
-            bytes_written = snprintf(p_str_buf, buf_len, "%d", *((zb_int32_t*)p_attr));
-            break;
+	case ZB_ZCL_ATTR_TYPE_S32:
+		bytes_written = snprintf(p_str_buf, buf_len, "%d",
+					 *((zb_int32_t*)p_attr));
+		break;
 
-        /* String */
-        case ZB_ZCL_ATTR_TYPE_CHAR_STRING:
-            string_len = p_attr[0];
-            p_attr++;
+	/* String */
+	case ZB_ZCL_ATTR_TYPE_CHAR_STRING:
+		string_len = p_attr[0];
+		p_attr++;
 
-            if ((buf_len - bytes_written) < (string_len + 1))
-            {
-                return -1;
-            }
+		if ((buf_len - bytes_written) < (string_len + 1)) {
+			return -1;
+		}
 
-            for (i = 0; i < string_len; i++)
-            {
-                /*lint -save -e661 */
-                p_str_buf[bytes_written + i] = ((char *)p_attr)[i];
-                /*lint -restore */
-            }
-            p_str_buf[bytes_written + i] = '\0';
-            bytes_written += string_len + 1;
-            break;
+		for (i = 0; i < string_len; i++) {
+			p_str_buf[bytes_written + i] = ((char *)p_attr)[i];
+		}
+		p_str_buf[bytes_written + i] = '\0';
+		bytes_written += string_len + 1;
+		break;
 
-        case ZB_ZCL_ATTR_TYPE_IEEE_ADDR:
-            /*lint -e661 -e662 -save */
-            bytes_written = to_hex_str(p_str_buf, buf_len, (const uint8_t *)p_attr, sizeof(zb_64bit_addr_t), true);
-            /*lint -restore */
-            break;
+	case ZB_ZCL_ATTR_TYPE_IEEE_ADDR:
+		/*lint -e661 -e662 -save */
+		bytes_written = to_hex_str(p_str_buf, buf_len,
+					   (const u8_t *)p_attr,
+					   sizeof(zb_64bit_addr_t), true);
+		/*lint -restore */
+		break;
 
-        default:
-            bytes_written = snprintf(p_str_buf, buf_len, "Value type 0x%x unsupported", attr_type);
-            break;
-    }
+	default:
+		bytes_written = snprintf(p_str_buf, buf_len,
+					 "Value type 0x%x unsupported",
+					 attr_type);
+		break;
+	}
 
-    return bytes_written;
+	return bytes_written;
 }
 
-int sscan_uint8(const char * p_bp, uint8_t * p_u8)
+int sscan_uint8(const char * p_bp, u8_t * p_u8)
 {
-    unsigned int value;
+	/* strtoul() used as a replacement for lacking sscanf(),
+	 * first character is tested to make sure that is a digit.
+	 */
+	unsigned long value = 0xFFFF;
+	value = strtoul(p_bp, NULL, 10);
 
-    if (!sscanf(p_bp, "%u", &value) || (value > 0xff))
-    {
-        return 0;
-    }
+	if ((value > UINT8_MAX) || (p_bp[0] < '0') || (p_bp[0] > '9')) {
+		return 0;
+	}
 
-    *p_u8 = value & 0xFF;
+	*p_u8 = value & 0xFF;
 
-    return 1;
+	return 1;
 }
 
-void print_hexdump(nrf_cli_t const * p_cli,
-                   const uint8_t * p_in, uint8_t size,
-                   bool reverse)
+void print_hexdump(const struct shell *shell, const u8_t * p_in, u8_t size,
+		   bool reverse)
 {
-    char addr_buf[2*size + 1];
-    int bytes_written = 0;
+	char addr_buf[2 * size + 1];
+	int bytes_written = 0;
 
-    memset(addr_buf, 0, sizeof(addr_buf));
+	memset(addr_buf, 0, sizeof(addr_buf));
 
-    bytes_written = to_hex_str(addr_buf, (uint16_t)sizeof(addr_buf), p_in, size, reverse);
-    if (bytes_written < 0)
-    {
-        nrf_cli_fprintf(p_cli, NRF_CLI_ERROR, "%s", "Unable to print hexdump");
-    }
-    else
-    {
-        nrf_cli_fprintf(p_cli, NRF_CLI_NORMAL, "%s", addr_buf);
-    }
+	bytes_written = to_hex_str(addr_buf, (u16_t)sizeof(addr_buf), p_in,
+				   size, reverse);
+	if (bytes_written < 0) {
+		shell_error(shell, "%s", "Unable to print hexdump");
+	} else {
+		shell_print(shell, "%s", addr_buf);
+	}
 }
