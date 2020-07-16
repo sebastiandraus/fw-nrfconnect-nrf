a902d29a2de171f41f71c3550079b81d88c43842
Sebastian Draus <sebastian.draus@nordicsemi.no>
2020-07-16 09:41:32 +0200

    Refactor ping command to be thread-safe

--- subsys/zigbee/cli/zigbee_cli_cmd_zcl.h
+++ subsys/zigbee/cli/zigbee_cli_cmd_zcl.h
@@ -14,4 +14,18 @@ int cmd_zb_subscribe(const struct shell *shell, size_t argc, char **argv);
 int cmd_zb_generic_cmd(const struct shell *shell, size_t argc, char **argv);
 int cmd_zb_zcl_raw(const struct shell *shell, size_t argc, char **argv);
 
+/* Structure used to pass informations required to send ZCL frame. */
+typedef struct zcl_packet_info_s {
+	zb_bufid_t buffer;
+	zb_uint8_t *ptr;
+	zb_addr_u *dst_addr;
+	zb_uint8_t dst_addr_mode;
+	zb_uint8_t dst_ep;
+	zb_uint8_t ep;
+	zb_uint16_t prof_id;
+	zb_uint16_t cluster_id;
+	zb_callback_t cb;
+	zb_bool_t disable_aps_ack;
+} zcl_packet_info_t;
+
 #endif /* ZIGBEE_CLI_CMD_ZCL_H__ */
