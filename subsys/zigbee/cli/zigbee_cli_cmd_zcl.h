8870b4edb8dd8ecfdeab3041d36d8eabeb9c9532
Sebastian Draus <sebastian.draus@nordicsemi.no>
2020-07-20 08:47:29 +0200

    zigbee: Port the rest of the ZCL commands of Zigbee CLI
    ...
    
    Signed-off-by: Sebastian Draus <sebastian.draus@nordicsemi.no>

--- subsys/zigbee/cli/zigbee_cli_cmd_zcl.h
+++ subsys/zigbee/cli/zigbee_cli_cmd_zcl.h
@@ -8,6 +8,12 @@
 #define ZIGBEE_CLI_CMD_ZCL_H__
 
 int cmd_zb_ping(const struct shell *shell, size_t argc, char **argv);
+int cmd_zb_readattr(const struct shell *shell, size_t argc, char **argv);
+int cmd_zb_writeattr(const struct shell *shell, size_t argc, char **argv);
+int cmd_zb_subscribe_on(const struct shell *shell, size_t argc, char **argv);
+int cmd_zb_subscribe_off(const struct shell *shell, size_t argc, char **argv);
+int cmd_zb_generic_cmd(const struct shell *shell, size_t argc, char **argv);
+int cmd_zb_zcl_raw(const struct shell *shell, size_t argc, char **argv);
 
 /* Structure used to pass information required to send ZCL frame. */
 struct zcl_packet_info {
